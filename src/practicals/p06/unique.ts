3b4029b30b6d3f83182018e4933a91ed9b8e7d1c
IkrtI <minipro787@gmail.com>
2026-01-21 15:16:09 +0700

    ssss

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -3,7 +3,13 @@ export function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
   let arr = [];
   arr = arr1.filter((a) => !arr2.includes(a));
   arr2.filter((a) => !arr1.includes(a)).forEach(a=>arr.push(a))
-  return arr.sort();
+  let rarr :number[]=[]
+  for (let index = 0; index < arr.length; index++) {
+    const num = arr[index];
+    if(rarr.includes(num)) continue;
+    rarr.push(num)
+  }
+  return rarr.sort();
 }
 
 const arr1 = [1, 2, 3, 4];
