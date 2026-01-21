ce81bcbac78e65fe98a9b5dbd9a5e193a92e090f
IkrtI <minipro787@gmail.com>
2026-01-21 14:16:02 +0700

    xxx

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,13 @@
 // Write your code below
+let input: string = process.argv[2];
+let number: number = Number(input);
+
+if ((!input || !number || number <= -1) && number !== 0) {
+  process.exit();
+}
+let l = "";
+for (let index = 0; index < number; index++) {
+  let num = index + 1;
+  console.log(`${" ".repeat(number - num)}${num}${l}`);
+  l = `${num}${l}`;
+}
