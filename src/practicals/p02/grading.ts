6ce8fa895ff0d5bc15f8cc8234103d98b91e74a4
IkrtI <minipro787@gmail.com>
2026-01-21 15:26:31 +0700

    dddd

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,19 +1,16 @@
 // Write your code below
-let input = process.argv.slice(2)
-let number = Number(input);
-
-if ((!number  || number <= -1) && number !>= 0) {
-  console.log("Invalid input");
-} else if (number <= 49) {
-  console.log("Grade is F");
-} else if (number <= 59) {
-  console.log("Grade is D");
-} else if (number <= 69) {
-  console.log("Grade is C");
-} else if (number <= 79) {
-  console.log("Grade is B");
-} else if (number <= 100) {
-  console.log("Grade is A");
+const input = process.argv.slice(2)
+const number = Number(input)
+if (number >= 0 && number <= 49) {
+    console.log("Grade is F")
+} else if (number >= 0 && number <= 59) {
+    console.log("Grade is D")
+} else if (number >= 0 && number <= 69) {
+    console.log("Grade is C")
+} else if (number >= 0 && number <= 79) {
+    console.log("Grade is B")
+} else if (number >= 0 && number <= 100) {
+    console.log("Grade is A")
 } else {
-  console.log("Invalid input");
+    console.log("Invalid input")
 }
