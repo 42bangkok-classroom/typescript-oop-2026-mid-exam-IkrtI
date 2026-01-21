2daa0a9359522b210c14f2da0bf8670ecdebeb3f
IkrtI <minipro787@gmail.com>
2026-01-21 16:22:51 +0700

    ssss

--- src/practicals/p09/read-json.ts
+++ src/practicals/p09/read-json.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 
 export async function readJson(fileName: string): unknown[] {
-  let x = fs.readFileSync("src/practicals/p09/datasets/products.json");
+  let x = fs.readFileSync("src/practicals/p09/datasets/" + fileName);
+  console.log(x);
 }
