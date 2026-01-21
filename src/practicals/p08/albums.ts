52751f125bd911e024ca5bf7de0718ab65b90d62
IkrtI <minipro787@gmail.com>
2026-01-21 16:13:22 +0700

    sssss

--- src/practicals/p08/albums.ts
+++ src/practicals/p08/albums.ts
@@ -27,21 +27,17 @@ export async function mapPhotoToAlbum(
   userIds?: number[]
 ): Promise<FullAlbum[]> {
   if (!userIds || userIds.length == 0) return [];
-  let photos: Photo[] = (await axios({
-    url: PHOTOS_URL,
-  })
+  let photos: Photo[] = await axios.get(PHOTOS_URL)
     .then((res) => res.data)
     .catch((x) => {
       return [];
-    })) as Photo[];
+    })
   if (!photos) return [];
-  let album: Album[] = (await axios({
-    url: ALBUMS_URL,
-  })
-    .then((res) => res.data)
-    .catch((x) => {
-      return [];
-    })) as Album[];
+  let album: Album[] = await axios.get(ALBUMS_URL)
+      .then((res) => res.data)
+      .catch((x) => {
+        return [];
+      })
   if (!album) return [];
   const Full: FullAlbum[] = [];
   for (let index = 0; index < userIds.length; index++) {
@@ -64,4 +60,4 @@ export async function mapPhotoToAlbum(
   return Full;
 }
 
-mapPhotoToAlbum([1])
+mapPhotoToAlbum([1]);
